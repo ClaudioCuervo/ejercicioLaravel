2e3f7000e32b4c9341e781ca4673dd16d7d3cbe5
Claudio Cuervo Gayoso <claudiocuervogayoso@gmail.com>
2021-06-22 19:28:40 +0200

    segundo commit

---
+++ resources/views/juego/form.blade.php
@@ -0,0 +1,1 @@
+formulario datos en comun
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JuegoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Route::get('/juego', function () {
+    return view('juego.index');
+});
+
+Route::get('juego/create',[JuegoController::class,'create']);
+*/
+
+Route::resource('juego',JuegoController::class);
+
